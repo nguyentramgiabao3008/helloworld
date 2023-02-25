d5a250fa9d7f60208dc9e442d4774d89bb375860
Nguyễn Trầm Gia Bảo <https://github.com/nguyentramgiabao3008/Smart-home.git>
2023-02-26 00:34:08 +0700

    sua lai ai do da conflict

--- about.cpp
+++ about.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
     cout<<"NGUYENTRAMGIABAO la nguoi code nhung cai nay chia se cho moi nguoi";
-    cout<<"co y tao conflict ahihi";
+    cout<<"ua ai conflict vay";
     return 0;
 }
